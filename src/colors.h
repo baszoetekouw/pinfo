9db8c447266577d888b1d176de7b48e0ebca7a3f
bas <bas@ea4b0d59-4df7-0310-a9f9-bf8cbe41ce66>
2005-07-25 20:47:44 +0000

    Added copyright messages to all source files.
    git-svn-id: svn://svn.debian.org/svn/pinfo/pinfo/trunk@24 ea4b0d59-4df7-0310-a9f9-bf8cbe41ce66

--- src/colors.h
+++ src/colors.h
@@ -1,3 +1,24 @@
+/***************************************************************************
+ *  Pinfo is a ncurses based lynx style info documentation browser
+ *
+ *  Copyright (C) 1999  Przemek Borys <pborys@dione.ids.pl>
+ *  Copyright (C) 2005  Bas Zoetekouw <bas@debian.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of version 2 of the GNU General Public License as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *  USA
+ ***************************************************************************/
+
 #ifndef __COLORS_H
 #define __COLORS_H
 
