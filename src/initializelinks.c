8de564095b4bd64f71aaebbefa9066dc66bf559c
Bas Zoetekouw <bas@debian.org>
2005-09-08 21:01:50 +0000

    Fixed scanning for node links of the form:  * Rules: Rpcalc Rules.  Grammar Rules for rpcalc, with explanation. (example taken from bison info page).  These weren't detected because finddot() looked for the last occurrence of a '.' (or ',' or '\t', or '\n'), instead of the first one. This fixes Debian bug #235134.
    git-svn-id: svn://scm.alioth.debian.org/svn/pinfo/pinfo/trunk@217 ea4b0d59-4df7-0310-a9f9-bf8cbe41ce66

--- src/initializelinks.c
+++ src/initializelinks.c
@@ -153,8 +153,8 @@ finddot(char *str, int mynote)
 			return 0;
 		ptr++;
 	}
-	end[0] = strrchr(str, '.');	/* nodename entry may end with dot, comma */
-	end[1] = strrchr(str, ',');	/* tabulation, or newline */
+	end[0] = strchr(str, '.');	/* nodename entry may end with dot, comma */
+	end[1] = strchr(str, ',');	/* tabulation, or newline */
 	if (!mynote)
 	{
 		end[2] = strchr(str, '\t');
@@ -211,7 +211,7 @@ findemailstart(char *str)
 	return 0;
 }
 
-	void
+void
 initializelinks(char *line1, char *line2, int line)
 {
 	char *tmp;
