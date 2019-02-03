baszoetekouw/pinfo

commit 1c0a1f0d12f91bb45eb68b71ec502709120d62e8

Fix gcc warnings (-Wall -Wextra)

src/filehandling_functions.c
@@ -262,6 +262,12 @@ read_item(FILE * id, char **type, char ***buf, long *lines)
 
 	freeitem(type, buf, lines);	/* free previously allocated memory */
 
+	/* set number of lines to 0 */
+	Lines = 0;
+
+	/* initial buffer allocation */
+	Buf = xmalloc(sizeof(char **));
+
 	/* seek precisely on the INFO_TAG (the seeknode function may be imprecise
 	 * in combination with some weird tag_tables).  */
 	while (fgetc(id) != INFO_TAG);
@@ -270,15 +276,12 @@ read_item(FILE * id, char **type, char ***buf, long *lines)
 
 	/* allocate and read the header line */
 	Type = xmalloc(1024);
-	fgets(Type, 1024, id);
+	if (fgets(Type, 1024, id)==NULL)
+	{
+		/* nothing to do */
+		return;
+	}
 	Type = xrealloc(Type, strlen(Type) + 1);
-
-	/* set number of lines to 0 */
-	Lines = 0;
-
-	/* initial buffer allocation */
-	Buf = xmalloc(sizeof(char **));
-
 	/* now iterate over the lines */
 	do
 	{
@@ -465,7 +468,10 @@ seek_indirect(FILE * id)
 			}
 		seek_pos = ftell(id) - 2;
 		fgetc(id);
-		fgets(type, 1024, id);
+		if (fgets(type, 1024, id)==0)
+		{
+			finito = 1;
+		}
 		if (strncasecmp("Indirect:", type, strlen("Indirect:")) == 0)
 		{
 			finito = 1;
@@ -539,8 +545,7 @@ seek_tag_table(FILE * id,int quiet)
 			if (feof(id))
 				break;
 		}
-		fgets(type, 1024, id);
-		if (strncasecmp("Tag Table:", type, strlen("Tag Table:")) == 0)
+		if (fgets(type, 1024, id)==NULL || strncasecmp("Tag Table:", type, strlen("Tag Table:")) == 0)
 		{
 			finito = 1;
 		}
@@ -589,7 +594,7 @@ opendirfile(int number)
 	char command[1128];		/* holds command to evaluate for decompression of file */
 	int i, j;
 	char *tmpfilename = NULL;
-	int *fileendentries = xmalloc(infopathcount * sizeof(int));
+	size_t *fileendentries = xmalloc(infopathcount * sizeof(*fileendentries)); /* should really be off_t, but a signed type really doesn't make sense here */
 	int dir_found = 0;
 	int dircount = 0;
 	int lang_found;
@@ -627,7 +632,7 @@ opendirfile(int number)
 			{
 				fclose(id);
 				builddircommand(command, suffixes[j].command, buf, tmpfilename);
-				system(command);
+				xsystem(command);
 				lstat(tmpfilename, &status);
 				fileendentries[dircount] = status.st_size;
 				dircount++;
@@ -656,7 +661,7 @@ opendirfile(int number)
 				{
 					fclose(id);
 					builddircommand(command, suffixes[j].command, buf, tmpfilename);
-					system(command);
+					xsystem(command);
 					lstat(tmpfilename, &status);
 					fileendentries[dircount] = status.st_size;
 					dircount++;
@@ -675,7 +680,7 @@ opendirfile(int number)
 	if (id)
 	{
 		char *tmp;
-		long filelen, l;
+		size_t filelen, l;
 		int aswitch = 0;
 		int firstswitch = 0;
 		dircount = 0;
@@ -685,7 +690,12 @@ opendirfile(int number)
 
 		tmp = xmalloc(filelen);
 		fseek(id, 0, SEEK_SET);
-		fread(tmp, 1, filelen, id);
+		if (fread(tmp, 1, filelen, id)!=filelen)
+		{
+			printf(_("Error while reading file '%s'"), tmp);
+			closeprogram();
+			exit(1);
+		}
 		fclose(id);
 		id = fopen(tmpfilename, "w");
 		for (l = 0; l < filelen; l++)
@@ -825,7 +835,7 @@ openinfo(char *filename, int number)
 						}
 				}
 				buildcommand(command, suffixes[j].command, buf, tmpfilename);
-				system(command);
+				xsystem(command);
 				id = fopen(tmpfilename, "r");
 				if (id)
 				{

.gitignore
@@ -1,4 +1,5 @@
 *~
+*.o
 Makefile
 Makefile.in
 
@@ -65,6 +66,8 @@ Makefile.in
 /doc/stamp-vti
 
 /macros/macros.dep
+
 /src/.deps/
+/src/pinfo
 /src/pinforc
 

src/mainfunction.c
@@ -239,7 +239,7 @@ work(char ***message, char **type, long *lines, FILE * id, int tag_table_pos)
 				attrset(normal);
 
 				myendwin();
-				system("clear");
+				xsystem("clear");
 				mypipe = popen(token, "w");	/* open mypipe */
 				if (mypipe != NULL)
 				{
@@ -353,8 +353,8 @@ work(char ***message, char **type, long *lines, FILE * id, int tag_table_pos)
 						starttokenpos = ftell(fd);
 
 						tmp = xmalloc(filelen - starttokenpos + 10);	/* read data */
-						fread(tmp, 1, filelen - starttokenpos, fd);
-						tmp[filelen - starttokenpos + 1] = 0;
+						size_t num_read = fread(tmp, 1, filelen - starttokenpos, fd);
+						tmp[num_read] = 0;
 
 						tokenpos = regexp_search(token, tmp);	/* search */
 
@@ -446,8 +446,8 @@ work(char ***message, char **type, long *lines, FILE * id, int tag_table_pos)
 
 					/* read data */
 					tmp = xmalloc(filelen - starttokenpos + 10);
-					fread(tmp, 1, filelen - starttokenpos, id);
-					tmp[filelen - starttokenpos + 1] = 0;
+					size_t num_read = fread(tmp, 1, filelen - starttokenpos, id);
+					tmp[num_read] = 0;
 
 					/* search */
 					tokenpos = regexp_search(token, tmp);
@@ -1021,7 +1021,7 @@ skip_search:
 								strcat(tempbuf, " ");
 								strcat(tempbuf, hyperobjects[cursor].node);
 								myendwin();
-								system(tempbuf);
+								xsystem(tempbuf);
 								doupdate();
 								xfree(tempbuf);
 							}
@@ -1032,7 +1032,7 @@ skip_search:
 								strcat(tempbuf, " ");
 								strcat(tempbuf, hyperobjects[cursor].node);
 								myendwin();
-								system(tempbuf);
+								xsystem(tempbuf);
 								doupdate();
 								xfree(tempbuf);
 							}
@@ -1043,8 +1043,8 @@ skip_search:
 								strcat(tempbuf, " ");
 								strcat(tempbuf, hyperobjects[cursor].node);
 								myendwin();
-								system("clear");
-								system(tempbuf);
+								xsystem("clear");
+								xsystem(tempbuf);
 								doupdate();
 								xfree(tempbuf);
 							}

src/manual.c
@@ -248,7 +248,7 @@ handlemanual(char *name)
 	FILE *id;
 
 	char manualname[256];
-	char cmd[256];
+	char cmd[4096];
 	char *raw_tempfilename = 0;
 	char *apropos_tempfilename = 0;
 
@@ -323,7 +323,7 @@ handlemanual(char *name)
 				{
 					printf(_("Apropos pages:\n"));
 					snprintf(cmd, 255, "apropos %s|cat %s", name, StderrRedirection);
-					system(cmd);
+					xsystem(cmd);
 				}
 				return 1;
 			}
@@ -440,12 +440,15 @@ handlemanual(char *name)
 	 ****************************************************************************/
 #endif
 	if (!plain_apropos)
+	{
 		snprintf(cmd, 255, "man %s %s %s > %s",
 				ManOptions,
 				name,
 				StderrRedirection,
 				tmpfilename1);
-	if ((plain_apropos) ||(system(cmd) != 0))
+	}
+
+	if (plain_apropos || (system_check(cmd) != 0))
 	{
 		if (!plain_apropos)
 		{
@@ -457,8 +460,8 @@ handlemanual(char *name)
 		{
 			printf(_("Calling apropos \n"));
 			apropos_tempfilename = make_tempfile();
-			snprintf(cmd, 255, "apropos %s > %s", name, apropos_tempfilename);
-			if (system(cmd) != 0)
+			snprintf(cmd, 4096, "apropos %s > %s", name, apropos_tempfilename);
+			if (system_check(cmd) != 0)
 			{
 				printf(_("Nothing appropiate\n"));
 				unlink(apropos_tempfilename);
@@ -511,7 +514,7 @@ handlemanual(char *name)
 			if (return_value != -2)
 			{
 				construct_manualname(manualname, return_value);
-				snprintf(cmd, 255, "man %s %s %s %s > %s",
+				snprintf(cmd, 4096, "man %s %s %s %s > %s",
 						ManOptions,
 						manuallinks[return_value].section,
 						manualname,
@@ -548,13 +551,13 @@ handlemanual(char *name)
 				 */
 				historical = 1;
 			}
-			system(cmd);
+			xsystem(cmd);
 			stat(tmpfilename2, &statbuf);
 			if (statbuf.st_size > 0)
 			{
 				snprintf(cmd, 255, "mv %s %s", tmpfilename2, tmpfilename1);
 				/* create tmp file containing man page */
-				system(cmd);
+				xsystem(cmd);
 				/* open man page */
 				id = fopen(tmpfilename1, "r");
 				if (id != NULL)
@@ -1074,7 +1077,7 @@ manualwork()
 				attrset(normal);
 
 				myendwin();
-				system("clear");
+				xsystem("clear");
 				/* open mypipe */
 				mypipe = popen(token, "w");
 				if (mypipe != NULL)
@@ -1398,7 +1401,7 @@ skip_search:
 							buflen = strlen(tempbuf);
 							construct_manualname(tempbuf + buflen, selected);
 							myendwin();
-							system(tempbuf);
+							xsystem(tempbuf);
 							doupdate();
 							xfree(tempbuf);
 						}
@@ -1411,7 +1414,7 @@ skip_search:
 							buflen = strlen(tempbuf);
 							construct_manualname(tempbuf + buflen, selected);
 							myendwin();
-							system(tempbuf);
+							xsystem(tempbuf);
 							doupdate();
 							xfree(tempbuf);
 						}
@@ -1424,7 +1427,7 @@ skip_search:
 							buflen = strlen(tempbuf);
 							construct_manualname(tempbuf + buflen, selected);
 							myendwin();
-							system(tempbuf);
+							xsystem(tempbuf);
 							doupdate();
 							xfree(tempbuf);
 						}

src/pinfo.c
@@ -639,6 +639,7 @@ checksu()
 {
 	struct passwd *pswd;
 	struct group *grwd;
+	int result = 0;
 
 	if (!getegid() || !getgid())
 	{
@@ -654,13 +655,13 @@ checksu()
 		else
 		{
 			if (!getgid() && !getuid())
-				setgid(grwd->gr_gid);
+				result = setgid(grwd->gr_gid);
 			else
-				setegid(grwd->gr_gid);
+				result = setegid(grwd->gr_gid);
 		}
 	}
 
-	if (!geteuid() || !getuid())
+	if (result==0 && (!geteuid() || !getuid()) )
 	{
 		pswd = getpwnam(safe_user);
 		if (!pswd)
@@ -674,10 +675,16 @@ checksu()
 		else
 		{
 			if (!getuid())
-				setuid(pswd->pw_uid);
+				result = setuid(pswd->pw_uid);
 			else
-				seteuid(pswd->pw_uid);
+				result = seteuid(pswd->pw_uid);
 		}
+	}
+
+	if (result != 0)
+	{
+		printf(_("Unable to drop root privileges: %s"), strerror(errno));
+		exit(-1);
 	}
 
 }

src/utils.c
@@ -190,6 +190,32 @@ xrealloc(void *ptr, size_t size)
 	return value;
 }
 
+int
+system_check(const char *command)
+{
+	if (command==NULL)
+	{
+		return -1;
+	}
+	int result = system(command);
+	if (WIFEXITED(result))
+	{
+		return WEXITSTATUS(result);
+	}
+	return -1;
+}
+
+void
+xsystem(const char *command)
+{
+	int result = system_check(command);
+	if (result!=0)
+	{
+		printf(_("Failed to execute command '%s': %i"), command, result);
+		exit(2);
+	}
+}
+
 void
 initlocale()
 {
@@ -301,7 +327,7 @@ closeprogram()
 	if (curses_open)
 		myendwin();
 	if (ClearScreenAtExit)
-		system("clear");
+		xsystem("clear");
 	else
 		printf("\n");
 	if (tmpfilename1)

src/utils.h
@@ -45,6 +45,10 @@ void xfree (void *ptr);
 void *xmalloc (size_t size);
 /* realloc() wrapper */
 void *xrealloc (void *ptr, size_t size);
+/* system(), but return sane error code */
+int system_check(const char *command);
+/* safe, error-checking, command execution */
+void xsystem(const char *command);
 /* initializes GNU locales */
 void initlocale ();
 /* checks if file name does not cause secuirity problems */

commit 7d9c62c2a4ba968cfe04efc3baf9ba8f21b03fbe

Fix bug introduced by 1c0a1f0d12f91bb45eb68b71ec502709120d62e8 which caused a segfault on files without indirect references or tag table

src/filehandling_functions.c
@@ -444,6 +444,7 @@ load_tag_table(char **message, long lines)
 	qsort(&tag_table[1], TagTableEntries, sizeof(TagTable), qsort_cmp);
 }
 
+/* TODO: seek_indirect() and seek_tag_table() are almost identical: remove duplicate code */
 int
 seek_indirect(FILE * id)
 {
@@ -471,7 +472,13 @@ seek_indirect(FILE * id)
 		fgetc(id);
 		if (fgets(type, 1024, id)==0)
 		{
-			finito = 1;
+			/* we're at the end of the file and haven't found any indirect refs. so bail out */
+			if (type)
+			{
+				xfree(type);
+				type = 0;
+			}
+			return 0;
 		}
 		if (strncasecmp("Indirect:", type, strlen("Indirect:")) == 0)
 		{
@@ -541,10 +548,16 @@ seek_tag_table(FILE * id,int quiet)
 			}
 		}
 		seek_pos = ftell(id) - 2;
-		while (fgetc(id) != '\n')
+		while (fgetc(id) != '\n');
+		if (fgets(type, 1024, id)==NULL)
 		{
-			if (feof(id))
-				break;
+			/* we're at the end of the file and haven't found a tag table. so bail out */
+			if (type)
+			{
+				xfree(type);
+				type = 0;
+			}
+			return 2;
 		}
 		if (fgets(type, 1024, id)==NULL || strncasecmp("Tag Table:", type, strlen("Tag Table:")) == 0)
 		{